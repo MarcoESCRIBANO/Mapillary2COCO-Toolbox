572094169a4ae5f8c526de55e3fac7e0d01cfdbf
Marco ESCRIBANO <44638661+MarcoESCRIBANO@users.noreply.github.com>
2024-05-30 15:29:16 +0200

    Update showAnnotation.py

--- showAnnotation.py
+++ showAnnotation.py
@@ -11,8 +11,12 @@ import pycococreatortools
 # MAP_DATASET_PATH = "/MapillaryVistas/"
 MAP_DATASET_PATH = "/MapillaryVistasResized/"
 
-TRAIN_VAL_PATH = "training"
-# TRAIN_VAL_PATH = "validation"
+# TRAIN_VAL_PATH = "training"
+TRAIN_VAL_PATH = "validation"
+
+# IMAGE_NAME = "" means all images 
+# It can be a list of images ex: IMAGE_NAME = "_1Gn_xkw7sa_i9GU4mkxxQ.jpg,E00oWA0zsmrvpDmZGxNw_w.jpg"
+IMAGE_NAME = ""  
 
 
 Map = COCO(MAP_DATASET_PATH + TRAIN_VAL_PATH + "/v2.0/instances_shape_" + TRAIN_VAL_PATH + "2020.json")
@@ -25,59 +29,62 @@ last_image_id = -1
 
 for ann in anns:
     image_id = ann["image_id"]
-    segm = ann['segmentation']
-    bbox = ann['bbox']
-    cat_id = ann['category_id']
-    color = cats[cat_id]["color"]
-    
-    bbox = np.array(ann["bbox"])
-    bbox[2:4] = bbox[0:2] + bbox[2:4]
-    
-    temp_image_path = MAP_DATASET_PATH + "RLE_label.png"
-    
-    assert sum(segm['counts']) == segm['size'][0] * segm['size'][1]
-
-    # Draw RLE label
-    label = np.zeros(segm['size'], np.uint8).reshape(-1)
-    ids = 0
-    value = 0
-    for c in segm['counts']:
-        label[ids: ids+c] = value
-        value = not value
-        ids += c
-    
-    label = label.reshape(segm['size'], order='F')
-    
-    cv2.imwrite(temp_image_path, label*255)
-
     image_info = Map.loadImgs(image_id)
     image_path = image_info[0]["file_name"]
-    image_path = MAP_DATASET_PATH + TRAIN_VAL_PATH + "/images/" + image_path
-    image = cv2.imread(image_path)
-    save_labeled_image_path = MAP_DATASET_PATH + "Labelled_" + TRAIN_VAL_PATH + "_image"+ str(image_id) + ".png"
-    
-    if (os.path.isfile(save_labeled_image_path) is False) or (image_id != last_image_id):
-        cv2.imwrite(save_labeled_image_path, image)
-        print("image_id: {}".format(image_id))
-    last_image_id = image_id  
+    if IMAGE_NAME == "" or image_path in IMAGE_NAME:
+        segm = ann['segmentation']
+        bbox = ann['bbox']
+        cat_id = ann['category_id']
+        color = cats[cat_id]["color"]
         
-    image_overlay = cv2.imread(temp_image_path)
-    print("image_overlay.shape: {}".format(image_overlay.shape))
-    overlay = image_overlay.copy()
-    h, w, c = overlay.shape
-    x, y, w, h = 0, 0, w, h
-    overlay = cv2.rectangle(overlay, (x, y), (x+w, y+h), tuple(map(operator.sub, (255,255,255), tuple(color))), -1) 
-    overlay = cv2.subtract(image_overlay, overlay)
-   
-    background = cv2.imread(save_labeled_image_path)
-    print("background.shape: {}".format(background.shape))
-    print("overlay.shape: {}".format(overlay.shape))
-    added_image = cv2.addWeighted(background, 1, overlay, 0.75, 0)
+        bbox = np.array(ann["bbox"])
+        bbox[2:4] = bbox[0:2] + bbox[2:4]
+        
+        temp_image_path = MAP_DATASET_PATH + "RLE_label.png"
+        
+        assert sum(segm['counts']) == segm['size'][0] * segm['size'][1]
+
+        # Draw RLE label
+        label = np.zeros(segm['size'], np.uint8).reshape(-1)
+        ids = 0
+        value = 0
+        for c in segm['counts']:
+            label[ids: ids+c] = value
+            value = not value
+            ids += c
+        
+        label = label.reshape(segm['size'], order='F')
+        
+        cv2.imwrite(temp_image_path, label*255)
+
+        
+        
+        image_path = MAP_DATASET_PATH + TRAIN_VAL_PATH + "/images/" + image_path
+        image = cv2.imread(image_path)
+        save_labeled_image_path = MAP_DATASET_PATH + "Labelled_" + TRAIN_VAL_PATH + "_image"+ str(image_id) + ".png"
+        
+        if (os.path.isfile(save_labeled_image_path) is False) or (image_id != last_image_id):
+            cv2.imwrite(save_labeled_image_path, image)
+            print("image_id: {}".format(image_id))
+        last_image_id = image_id  
+            
+        image_overlay = cv2.imread(temp_image_path)
+        print("image_overlay.shape: {}".format(image_overlay.shape))
+        overlay = image_overlay.copy()
+        h, w, c = overlay.shape
+        x, y, w, h = 0, 0, w, h
+        overlay = cv2.rectangle(overlay, (x, y), (x+w, y+h), tuple(map(operator.sub, (255,255,255), tuple(color))), -1) 
+        overlay = cv2.subtract(image_overlay, overlay)
     
-    cv2.rectangle(added_image, (int(bbox[0]), int(bbox[1])), 
-                  (int(bbox[2]), int(bbox[3])), tuple(color), 1)
-    
-    cv2.imwrite(save_labeled_image_path, added_image)
+        background = cv2.imread(save_labeled_image_path)
+        print("background.shape: {}".format(background.shape))
+        print("overlay.shape: {}".format(overlay.shape))
+        added_image = cv2.addWeighted(background, 1, overlay, 0.75, 0)
+        
+        cv2.rectangle(added_image, (int(bbox[0]), int(bbox[1])), 
+                    (int(bbox[2]), int(bbox[3])), tuple(color), 1)
+        
+        cv2.imwrite(save_labeled_image_path, added_image)
 
 
 
@@ -88,31 +95,34 @@ cats = Map.cats
 last_image_id = -1
 for i, ann in enumerate(anns):
     image_id = ann["image_id"]
-    segs = ann["segmentation"]
-    bbox = np.array(ann["bbox"])
-    bbox[2:4] = bbox[0:2] + bbox[2:4]
-    cat_id = ann['category_id']
-    color = cats[cat_id]["color"]
-    
     image_info = Map.loadImgs(image_id)
     image_path = image_info[0]["file_name"]
-    image_path = MAP_DATASET_PATH + TRAIN_VAL_PATH + "/images/" + image_path
-    
-    save_labeled_image_path = MAP_DATASET_PATH + "Labelled_" + TRAIN_VAL_PATH + "_image"+ str(image_id) + ".png"
+    if IMAGE_NAME == "" or image_path in IMAGE_NAME:
+        segs = ann["segmentation"]
+        bbox = np.array(ann["bbox"])
+        bbox[2:4] = bbox[0:2] + bbox[2:4]
+        cat_id = ann['category_id']
+        color = cats[cat_id]["color"]
+        
+        
+        
+        image_path = MAP_DATASET_PATH + TRAIN_VAL_PATH + "/images/" + image_path
+        
+        save_labeled_image_path = MAP_DATASET_PATH + "Labelled_" + TRAIN_VAL_PATH + "_image"+ str(image_id) + ".png"
 
-    if (os.path.isfile(save_labeled_image_path) is False) :
-        image = cv2.imread(image_path)
+        if (os.path.isfile(save_labeled_image_path) is False) :
+            image = cv2.imread(image_path)
+            cv2.imwrite(save_labeled_image_path, image)
+        last_image_id = image_id  
+
+        image = cv2.imread(save_labeled_image_path)
+
+        segs = [np.array(seg, np.int32).reshape((1, -1, 2))
+                for seg in segs]
+
+        for seg in segs: image = cv2.drawContours(image, seg, -1, tuple(color), 3)
+
+        cv2.rectangle(image, (int(bbox[0]), int(bbox[1])), 
+                    (int(bbox[2]), int(bbox[3])), tuple(color), 3)
+
         cv2.imwrite(save_labeled_image_path, image)
-    last_image_id = image_id  
-
-    image = cv2.imread(save_labeled_image_path)
-
-    segs = [np.array(seg, np.int32).reshape((1, -1, 2))
-            for seg in segs]
-
-    for seg in segs: image = cv2.drawContours(image, seg, -1, tuple(color), 1)
-
-    cv2.rectangle(image, (int(bbox[0]), int(bbox[1])), 
-                  (int(bbox[2]), int(bbox[3])), tuple(color), 1)
-
-    cv2.imwrite(save_labeled_image_path, image)
